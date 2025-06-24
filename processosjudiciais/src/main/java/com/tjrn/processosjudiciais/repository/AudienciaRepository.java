11d619d2d287abb57acd152c335c49b9c92e968c
mariacarolinass <m.carolina.soares1@gmail.com>
2025-06-24 05:03:08 -0300

    add query para repository

--- processosjudiciais/src/main/java/com/tjrn/processosjudiciais/repository/AudienciaRepository.java
+++ processosjudiciais/src/main/java/com/tjrn/processosjudiciais/repository/AudienciaRepository.java
@@ -3,14 +3,17 @@ package com.tjrn.processosjudiciais.repository;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.tjrn.processosjudiciais.model.Audiencia;
 
 @Repository
 public interface AudienciaRepository extends JpaRepository<Audiencia, Long> {
     
+    @Query("SELECT a FROM Audiencia a WHERE a.processo.comarca = :comarca AND a.data = :data")
     List<Audiencia> findByProcessoComarcaAndData(String comarca, LocalDate data);
-
+    
+    @Query("SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END FROM Audiencia a WHERE a.local = :local AND a.processo.vara = :vara")
     boolean existsByLocalAndProcessoVara(String local, String vara);
 
 }
--- processosjudiciais/src/main/java/com/tjrn/processosjudiciais/repository/ProcessoRepository.java
+++ processosjudiciais/src/main/java/com/tjrn/processosjudiciais/repository/ProcessoRepository.java
@@ -3,16 +3,20 @@ package com.tjrn.processosjudiciais.repository;
 import java.io.ObjectInputFilter.Status;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.tjrn.processosjudiciais.model.Processo;
 
 @Repository
 public interface ProcessoRepository extends JpaRepository<Processo, Long> {
-    
+
+    @Query("SELECT p FROM Processo p WHERE p.status = :status")
     List<Processo> findByStatus(Status status);
 
+    @Query("SELECT p FROM Processo p WHERE p.comarca = :comarca")
     List<Processo> findByComarca(String comarca);
     
+    @Query("SELECT p FROM Processo p WHERE p.status = :status AND p.comarca = :comarca")
     List<Processo> findByStatusAndComarca(Status status, String comarca);
 
 }
