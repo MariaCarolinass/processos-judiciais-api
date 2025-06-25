e0b6b0cf1bcf27dfa7442aead4d834f857baea9f
mariacarolinass <m.carolina.soares1@gmail.com>
2025-06-25 17:10:02 -0300

    atualizei método de adicionar audiencia

--- processosjudiciais/src/main/java/com/tjrn/processosjudiciais/controller/AudienciaController.java
+++ processosjudiciais/src/main/java/com/tjrn/processosjudiciais/controller/AudienciaController.java
@@ -2,20 +2,33 @@ package com.tjrn.processosjudiciais.controller;
 
 import java.time.LocalDate;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.tjrn.processosjudiciais.dto.AudienciaDTO;
+import com.tjrn.processosjudiciais.mapper.AudienciaMapper;
 import com.tjrn.processosjudiciais.model.Audiencia;
+import com.tjrn.processosjudiciais.model.Processo;
+import com.tjrn.processosjudiciais.repository.ProcessoRepository;
 import com.tjrn.processosjudiciais.service.AudienciaService;
 
 @RestController
 @RequestMapping("/api/v1/audiencias")
 public class AudienciaController {
 
+    @Autowired
     private final AudienciaService service;
+
+    @Autowired
+    private AudienciaMapper mapper;
+
+    @Autowired
+    private ProcessoRepository repository;
 
     public AudienciaController(AudienciaService audienciaService) {
         this.service = audienciaService;
@@ -34,8 +47,15 @@ public class AudienciaController {
     }
 
     @PostMapping
-    public Audiencia adicionarAudiencia(@RequestBody Audiencia audiencia) {
-        return service.save(audiencia);
+    public ResponseEntity<AudienciaDTO> adicionarAudiencia(@RequestBody AudienciaDTO dto) {
+        Audiencia entity = mapper.toEntity(dto);
+
+        Processo processo = repository.findById(dto.getProcessoId())
+            .orElseThrow(() -> new IllegalArgumentException("Processo com ID " + dto.getProcessoId() + " não encontrado."));
+        entity.setProcesso(processo);
+
+        Audiencia salvo = service.save(entity);
+        return ResponseEntity.ok(mapper.toDto(salvo));
     }
 
 }
