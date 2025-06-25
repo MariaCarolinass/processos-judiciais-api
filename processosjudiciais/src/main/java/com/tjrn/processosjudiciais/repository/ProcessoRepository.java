d4a542e28234efb482853248d1c30e3490c04484
mariacarolinass <m.carolina.soares1@gmail.com>
2025-06-25 17:11:15 -0300

    add repository para verificar se já existe num processo

--- processosjudiciais/src/main/java/com/tjrn/processosjudiciais/repository/ProcessoRepository.java
+++ processosjudiciais/src/main/java/com/tjrn/processosjudiciais/repository/ProcessoRepository.java
@@ -1,6 +1,6 @@
 package com.tjrn.processosjudiciais.repository;
 
-import java.io.ObjectInputFilter.Status;
+import com.tjrn.processosjudiciais.model.Status;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -19,4 +19,6 @@ public interface ProcessoRepository extends JpaRepository<Processo, Long> {
     @Query("SELECT p FROM Processo p WHERE p.status = :status AND p.comarca = :comarca")
     List<Processo> findByStatusAndComarca(Status status, String comarca);
 
+    boolean existsByNumProcesso(String numProcesso);
+    
 }
